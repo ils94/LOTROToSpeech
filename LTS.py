f8fe150f4b46b0e48195941d379fd102a8089f33
none <none@>
2023-08-29 17:14:49 -0300

    improved hotkey to start/stop TTS. hitting Ctrl+A while the LOTRO To Speech overlay is open, will show a window containing a preview of the OCR, that why, you can check if the OCR is in fact capturing the text correctly before initiating the TTS.

--- LTS.py
+++ LTS.py
@@ -79,7 +79,7 @@ def on_drag(event):
 
 
 def on_release(event):
-    global end_x, end_y, monitoring
+    global end_x, end_y
 
     if event.state & 0x4:
         end_x = canvas.canvasx(event.x)
@@ -87,51 +87,33 @@ def on_release(event):
 
         save_coordinates(start_x, start_y, end_x, end_y)
 
-        if not monitoring:
-            start_monitoring()
-
 
 def start_monitoring():
-    global monitoring
-
-    if not monitoring:
-        monitor_thread = threading.Thread(target=monitor_loop)
-        monitor_thread.setDaemon(True)
-        monitoring = True
-        monitor_thread.start()
+    monitor_thread = threading.Thread(target=monitor_loop)
+    monitor_thread.setDaemon(True)
+    monitor_thread.start()
 
 
-def start_tts_observer():
-    start_tts_thread = threading.Thread(target=start_tts)
-    start_tts_thread.setDaemon(True)
-    start_tts_thread.start()
-
-
-def start_tts():
+def start_stop_tts():
     global start
 
-    while True:
-        if keyboard.is_pressed("Ctrl"):
-            if keyboard.is_pressed("Alt"):
-                if start:
-                    start = False
+    if start:
+        start = False
 
-                    notification.notify(
-                        title="LOTRO To Speech",
-                        message="TTS is now OFF"
-                        # Specify the duration (in seconds) the notification should be displayed
-                    )
+        notification.notify(
+            title="LOTRO To Speech",
+            message="TTS is now OFF"
+            # Specify the duration (in seconds) the notification should be displayed
+        )
 
-                else:
-                    start = True
+    else:
+        start = True
 
-                    notification.notify(
-                        title="LOTRO To Speech",
-                        message="TTS is now ON"
-                        # Specify the duration (in seconds) the notification should be displayed
-                    )
-
-        time.sleep(0.1)
+        notification.notify(
+            title="LOTRO To Speech",
+            message="TTS is now ON"
+            # Specify the duration (in seconds) the notification should be displayed
+        )
 
 
 def is_image_on_screen():
@@ -153,8 +135,48 @@ def is_image_on_screen():
         return True
 
 
+def center_window(window, min_width, min_height):
+    # Get the screen width and height
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+
+    # Calculate the x and y coordinates for centering
+    x = (screen_width - min_width) // 2
+    y = (screen_height - min_height) // 2
+
+    # Set the window's geometry to center it on the screen
+    window.geometry(f"{min_width}x{min_height}+{x}+{y}")
+
+
+def ocr_preview(event):
+    global ocr_text_window, ocr_text_widget, text_ocr
+
+    if ocr_text_window is None or not ocr_text_window.winfo_exists():
+        ocr_text_window = tk.Toplevel(root)
+        ocr_text_window.title("OCR Result")
+        ocr_text_window.geometry("500x500")
+        ocr_text_window.iconbitmap("lotrotospeech.ico")
+        ocr_text_window.attributes("-topmost", True)
+
+        # Create and pack the Text widget
+        ocr_text_widget = tk.Text(ocr_text_window, wrap=tk.WORD)
+        ocr_text_widget.pack(fill=tk.BOTH, expand=True)
+
+        # Center the window on the screen with a minimum size
+        center_window(ocr_text_window, 500, 500)  # Adjust the minimum size as needed
+    else:
+        # If the window exists, bring it to the top (optional)
+        ocr_text_window.attributes("-topmost", True)
+
+    # Clear existing text
+    ocr_text_widget.config(state=tk.NORMAL)
+    ocr_text_widget.delete(1.0, tk.END)
+    ocr_text_widget.insert(tk.END, text_ocr)
+    ocr_text_widget.config(state=tk.DISABLED)
+
+
 def monitor_loop():
-    global start_x, start_y, end_x, end_y
+    global start_x, start_y, end_x, end_y, start, text_ocr
 
     while True:
         try:
@@ -174,7 +196,7 @@ def monitor_loop():
 
                 cleaned_text = re.sub(r'[^\w\s.!?;,\']', '', text_without_double_spaces)
 
-                print(cleaned_text)
+                text_ocr = cleaned_text
 
                 if start:
                     tts_engine(cleaned_text)
@@ -187,31 +209,32 @@ def monitor_loop():
 
 
 root = tk.Tk()
-root.title("LOTRO To Speech - Windows TTS Version")
+root.title("LOTRO To Speech - Elevenlab TTS Version")
 root.attributes("-alpha", 0.5)
 root.attributes("-topmost", True)
 root.state("zoomed")
 root.iconbitmap("lotrotospeech.ico")
+root.bind("<Control-a>", ocr_preview)
 
 canvas = tk.Canvas(root, cursor="cross")
 canvas.pack(fill=tk.BOTH, expand=True)
 
 rect = None
-monitoring = False
 start = False
+text_ocr = ""
+ocr_text_window = None
+ocr_text_widget = None
 start_x, start_y, end_x, end_y = load_coordinates()
 
 canvas.bind("<ButtonPress-1>", on_press)
 canvas.bind("<B1-Motion>", on_drag)
 canvas.bind("<ButtonRelease-1>", on_release)
 
-start_tts_observer()
+keyboard.add_hotkey("ctrl+alt", start_stop_tts)
 
 if start_x:
-
     rect = canvas.create_rectangle(start_x, start_y, end_x, end_y, fill=rect_color)
 
-    if not monitoring:
-        start_monitoring()
+start_monitoring()
 
 root.mainloop()
